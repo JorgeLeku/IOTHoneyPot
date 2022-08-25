93ffa305b54fd7911a366a86992163ca428a9809
Jorge Lekuona <43331910+JorgeLeku@users.noreply.github.com>
2022-08-25 21:01:45 +0200

    HoneyPotCreada

--- IoT_HoneyPot/simularYlogearDispositivoIoT.py
+++ IoT_HoneyPot/simularYlogearDispositivoIoT.py
@@ -1,33 +1,51 @@
-import sys
 import socket
+import sys
 import random
-import pickle
+
 sys.path.append('../API/')
 from apiArchivos import leerDatos, guardarDatos
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sck = socket.socket()
-host = socket.gethostname()
+respuestas = leerDatos('../IoT_Device_Searcher/datos/datosRespuestasPorDefecto.dat')
 
-def main ():
-    respuestasPredefinidas = leerDatos('../IoT_Device_Searcher/datos/datosRespuestasPorDefecto.dat')
-    ipsDetectadas = leerDatos('../IoT_HoneyPot/IPsConectadas/puerto8081.dat')
+def inicializarDatos():
+    puerto = int(input("Introducir puerto: "))
+    ipSrv = str(input("Introducir IP: "))
+    ip = (ipSrv, puerto)
+    print('inicializando servidor en la ip {} y puerto {}'.format(*ip))
+    sock.bind(ip)
+    sock.listen(1)
+    return(ip)
     
-    sck.bind((host, 8081))
-    
+def responderAConexion(connection, client_address, ip):
+    while True:
+        dato = connection.recv(16)
+        dt =+ dato
+        print('dato {!r}'.format(dato))
+        if dato:
+            print('Respondiendo al cliente')
+            dir, response = random.choice(list(respuestas.items()))
+            connection.sendall(response.content)
+        else:
+            print('sin datos del cliente', client_address)
+            break
+    return(dt, dato)
+
+def main():
+    ip = inicializarDatos()
     try:
         while True:
-            direcc, respuesta = random.choice(list(respuestasPredefinidas.items()))
-            conn, direcc = sck.accept()
-            print('IP '+ direcc + ' se ha conectado')
-            msg = conn.recv(16384)
-            print('Mensaje recibido: ' + msg)
-            ipsDetectadas.add(str(msg))
-            if b'login.cgi' in msg:
-                print('Intento de login')
-                print('IP: ' + str(direcc) + 'Respuesta: ' + str(respuesta))
-                conn.send(respuesta.content)
-            conn.close()
+            datos = leerDatos('../IoTHoneyPot/DatosObtenidos/puerto' + ip + '.dat')
+            ips = leerDatos('../IoTHoneyPot/IPsConectadas/puerto' + ip + '.dat')
+            print('A la espera')
+            conn, dirCli = sock.accept()
+            ips.add(dir)
+            print('Conexion recivida de', dirCli)
+            dt, dato = responderAConexion(conn, dirCli, ip, datos)
+            datos.add(dato)
+            guardarDatos('../IoTHoneyPot/IPsConectadas/puerto' + ip + '.dat', ips)
+            guardarDatos('../IoTHoneyPot/DatosObtenidos/puerto' + ip + '.dat', dt)
     except KeyboardInterrupt:
-        print('Interrumpido')
-    guardarDatos('../IoT_HoneyPot/IPsConectadas/puerto8081.dat', ipsDetectadas)
+        print("Cancelado. Terminando ejecución")
+
 main()
